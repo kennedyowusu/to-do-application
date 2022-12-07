c54b2a4aa46901ba9a3777f238b1c69279515c98
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-07 20:44:39 +0000

    build the UI

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import './style.css';
 
 function component() {
   const element = document.createElement('div');
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join([], ' ');
   element.classList.add('hello');
 
   return element;

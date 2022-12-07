f00c415ec02f3212f9e2847c17c38247b6b4404b
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-07 21:42:05 +0000

    pass data to the UI from the js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+/* eslint-disable no-unused-vars */
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join([], ' ');
-  element.classList.add('hello');
+const uiData = [
+  {
+    description: 'Buy Groceries',
+    completed: false,
+    index: 1,
+  },
 
-  return element;
-}
+  {
+    description: 'Go to the Library',
+    completed: true,
+    index: 2,
+  },
 
-document.body.appendChild(component());
+  {
+    description: 'Attend a Church Service',
+    completed: true,
+    index: 4,
+  },
+];
+
+const renderUIData = uiData.forEach((item) => {
+  const { description, completed, index } = item;
+
+  const dataPlaceHolder = `
+    <li class="todo-item" id=${index} item=${completed} >
+      <div class="box-list-more">
+        <input type="checkbox" name="todo" class="todo-checkbox" id="todoCheckbox">
+        <p class="todo-description">${description}</p>
+        <button class="more_vert">
+        <span class="material-icons">
+        more_vert
+        </span>
+      </button>
+      
+    </div>
+    
+  </li>
+  <hr class="divider" />
+  `;
+  document.querySelector('.todo-list').innerHTML += dataPlaceHolder;
+});
+
+window.onload = renderUIData;

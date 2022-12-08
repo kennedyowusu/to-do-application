3f34c40577fb474bbecb7b94d56945df1c4f1f3e
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-08 21:04:05 +0000

    refactor the index.js file to reflect what code is actually supposed to do

--- src/index.js
+++ src/index.js
@@ -7,25 +7,17 @@ const inputTaskFormField = document.getElementById('inputTaskFormField');
 
 const taskTodoHolder = document.getElementById('taskTodoHolder');
 
-let taskArray = [];
-
-const addUserTaskList = () => {
-  const taskEmptyObject = {};
-  taskEmptyObject.index = taskArray.length + 1;
-  taskEmptyObject.description = inputTaskFormField.value;
-  taskEmptyObject.completed = false;
-
-  taskArray.push(taskEmptyObject);
-};
-
-const saveUserTaskToLocalStorage = () => {
+const saveUserTaskToLocalStorage = (taskArray) => {
   localStorage.setItem('taskArray', JSON.stringify(taskArray));
 };
 
-const sendUserTaskToLocalStorage = () => {
+const generateUI = (taskArray) => {
   taskTodoHolder.innerHTML = '';
   taskArray.forEach((item) => {
-    const { description, index } = item;
+    const {
+      description,
+      index,
+    } = item;
 
     const dataPlaceHolder = `
     <li class="each-todo-item">
@@ -49,15 +41,12 @@ const sendUserTaskToLocalStorage = () => {
   deleteUserTaskFromList.forEach((element) => {
     element.addEventListener('click', () => {
       const elementIndex = taskArray.findIndex((isTask) => isTask.index === parseInt(element.dataset.id, 10));
-      const alreadyRemovedUserTask = (uniqueId) => {
-        taskArray.splice(uniqueId, 1);
-        for (let i = 0; i < taskArray.length; i += 1) {
-          taskArray[i].index = i + 1;
-        }
-        saveUserTaskToLocalStorage();
-        localStorage.setItem('taskArray', JSON.stringify(taskArray));
-      };
-      alreadyRemovedUserTask(elementIndex);
+      taskArray.splice(elementIndex, 1);
+      for (let i = 0; i < taskArray.length; i += 1) {
+        taskArray[i].index = i + 1;
+      }
+      saveUserTaskToLocalStorage(taskArray);
+      generateUI(taskArray);
     });
   });
 
@@ -69,8 +58,8 @@ const sendUserTaskToLocalStorage = () => {
       const inputDescId = taskArray.findIndex((isDesc) => isDesc.index === parseInt(inputDesc.dataset.id, 10));
       taskArray[inputDescId].description = inputDesc.value;
       const updateUserExistingTask = () => {
-        sendUserTaskToLocalStorage();
-        localStorage.setItem('taskArray', JSON.stringify(taskArray));
+        generateUI(taskArray);
+        saveUserTaskToLocalStorage(taskArray);
       };
       updateUserExistingTask();
     });
@@ -78,22 +67,29 @@ const sendUserTaskToLocalStorage = () => {
 };
 
 const retrieveUserSavedTaskFromLocalStorage = () => {
+  let retrievedTasks = [];
   if (localStorage.getItem('taskArray') !== null) {
-    const retrievedTask = JSON.parse(localStorage.getItem('taskArray'));
-
-    taskArray = retrievedTask;
+    retrievedTasks = JSON.parse(localStorage.getItem('taskArray'));
   }
-  sendUserTaskToLocalStorage();
+  return retrievedTasks;
 };
 
 window.addEventListener('load', () => {
-  retrieveUserSavedTaskFromLocalStorage();
+  const retrievedTasks = retrieveUserSavedTaskFromLocalStorage();
+  generateUI(retrievedTasks);
 });
 
 inputTaskFormField.addEventListener('keypress', (event) => {
+  const retrievedTasks = retrieveUserSavedTaskFromLocalStorage();
+
   if (event.key === 'Enter') {
-    addUserTaskList();
-    sendUserTaskToLocalStorage();
-    saveUserTaskToLocalStorage();
+    const taskEmptyObject = {};
+    taskEmptyObject.index = retrievedTasks.length + 1;
+    taskEmptyObject.description = inputTaskFormField.value;
+    taskEmptyObject.completed = false;
+
+    retrievedTasks.push(taskEmptyObject);
+    generateUI(retrievedTasks);
+    saveUserTaskToLocalStorage(retrievedTasks);
   }
 });
